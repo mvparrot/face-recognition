37ead525bcc7b404ba987ceca3d27f02aedf5573
Steff <srkob1@student.monash.edu>
2017-02-14 16:24:37 +1100

    glm modelling

--- regressionWIP.R
+++ regressionWIP.R
@@ -14,7 +14,7 @@ glmFits <- ALLmetaIMGFaces %>%
   split(.$FaceKey) %>%
   map_df(~ hitmiss(.)) %>%
   split(.$type) %>%
-  map(~ glm(hit ~ ., data = select(., -type, -file, -boxID)))
+  map(~ glm(hit ~ 0 + situation + . + visorhat*glasses, data = select(., -type, -file), binomial(link = "logit")))
 
 glmSummary <- glmFits %>%
   map(~ rename(cbind(rownames_to_column(cbind(as.data.frame(coef(summary(.)))))), variable = rowname))
@@ -30,7 +30,30 @@ glmPlot %>%
 
 ## Significance of variables by software
 glmPlot %>%
-  mutate(significant = `Pr(>|t|)` < 0.05) %>%
-  ggplot(aes(x=type, y=`Pr(>|t|)`)) +
+  mutate(significant = `Pr(>|z|)` < 0.05) %>%
+  mutate(`Pr(<|z|)` = 1 - `Pr(>|z|)`) %>%
+  ggplot(aes(x=type, y=`Pr(<|z|)`)) +  
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
   geom_col(aes(fill=significant)) + 
   facet_wrap(~ variable)
+
+## Significance of Estimated variable influences by software
+glmPlot %>%
+  mutate(significant = `Pr(>|z|)` < 0.05) %>%
+  ggplot(aes(x=type, y=Estimate)) + 
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  geom_col(aes(fill=significant)) + 
+  facet_wrap(~ variable, scales = "free_y")
+
+summary(ALLmetaIMGFaces)
+
+
+#################
+#WIP attempt to include type in model
+glmFits <- ALLmetaIMGFaces %>%
+  split(.$FaceKey) %>%
+  map_df(~ hitmiss(.)) %>% 
+  #split(.$type) %>%
+  map(~ glm(hit ~ 0 + type*., data = select(., -file), binomial(link = "logit")))
+
+
