ff9688326171f9edd0322982fd874fea398f9fec
Steff <srkob1@student.monash.edu>
2017-03-10 17:35:06 +1100

    Introduced possible emotion detection software script

---
+++ SoftwareRequestScripts/autoKairos.R
@@ -0,0 +1,21 @@
+library(httr)
+#set up and return git web address for id
+a <- GET(url="https://api.kairos.com/v2/analytics/{id}",
+          add_headers(`app_id` = "8078e093",
+                      `app_key` = "dd97f6d1afd17676f7130e180068b760")
+)
+
+#img.url = "The git img url"
+
+mybody = list(url = img.url)
+
+
+KairosAPI <- function(img.url){
+  GET(url="https://api.kairos.com/v2/analytics/{id}",
+      add_headers(`app_id` = "8078e093",
+                  `app_key` = "dd97f6d1afd17676f7130e180068b760")
+  )
+}
+
+         
+        
