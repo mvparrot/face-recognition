d779beb037d013356e4f6b6d46a2f577b9024cad
Steff <srkob1@student.monash.edu>
2017-03-13 11:22:04 +1100

    script to automate Kairos

--- SoftwareRequestScripts/autoKairos.R
+++ SoftwareRequestScripts/autoKairos.R
@@ -1,6 +1,6 @@
 library(httr)
 #set up and return git web address for id
-a <- GET(url="https://api.kairos.com/v2/analytics/{id}",
+a <- GET(url="https://api.kairos.com/v2/analytics/{https://github.com/srkob1/face-recognition/blob/master/TestFaces/face-947-1-Go.png}",
           add_headers(`app_id` = "8078e093",
                       `app_key` = "dd97f6d1afd17676f7130e180068b760")
 )
