ce6f46dfc921c1db4ce3c28f32ed12ee0cf73eb4
Steff <srkob1@student.monash.edu>
2017-07-18 11:00:08 +1000

    Change merge type

--- R/mergeData.R
+++ R/mergeData.R
@@ -7,12 +7,12 @@ library(purrr)
 
 
 #Load Data
-MicrosoftClassifiedFaces <- read.csv("SoftwareRequestScripts/MicrosoftClassifiedFaces.csv")
-AnimetricsClassifiedFaces <- read.csv("SoftwareRequestScripts/AnimetricsClassifiedFaces.csv")
-SkybiometryClassifiedFaces <- read.csv("SoftwareRequestScripts/SkybiometryClassifiedFaces.csv")
-GoogleClassifiedFaces <- read.csv("SoftwareRequestScripts/GoogleClassifiedFaces.csv")
-ManualClassifiedFaces <- read.csv("ManualClassifiedFaces.csv")
-ManualClassifiedScenes <- read.csv("ManualClassifiedScenes.csv")
+MicrosoftClassifiedFaces <- read_csv("data/ClassifiedFaces/MicrosoftClassifiedFaces.csv")
+AnimetricsClassifiedFaces <- read_csv("data/ClassifiedFaces/AnimetricsClassifiedFaces.csv")
+SkybiometryClassifiedFaces <- read_csv("data/ClassifiedFaces/SkybiometryClassifiedFaces.csv")
+GoogleClassifiedFaces <- read_csv("data/ClassifiedFaces/GoogleClassifiedFaces.csv")
+ManualClassifiedFaces <- read_csv("data/ManualClassifiedFaces.csv")
+ManualClassifiedScenes <- read_csv("data/ManualClassifiedScenes.csv")
 
 #Check all equal length
 #length(unique(ManualClassifiedScenes$file))
@@ -28,35 +28,34 @@ GoogleClassifiedFaces <- GoogleClassifiedFaces[!is.na(GoogleClassifiedFaces$roll
 
 #Manual Classified Faces
 ManualClassifiedFaces <- ManualClassifiedFaces %>%
-  mutate(type = "Manual", time.user.self=NA, time.sys.self=NA, time.elapsed=NA, ID=1:NROW(ManualClassifiedFaces))
+  dplyr::mutate(type = "Manual", time.user.self=NA, time.sys.self=NA, time.elapsed=NA, ID=1:NROW(ManualClassifiedFaces))
 
 #Merge data
 MicrosoftMerge <- MicrosoftClassifiedFaces %>% 
-  mutate(type = "Microsoft", ID=NA,
+  dplyr::mutate(type = "Microsoft", ID=NA,
          minX = faceRectangle.left, maxX = faceRectangle.left + faceRectangle.width,
          minY = faceRectangle.top, maxY = faceRectangle.top + faceRectangle.height) %>%
   dplyr::select(file, type, ID, time.user.self, time.sys.self, time.elapsed, minX, maxX, minY, maxY)  
 AnimetricsMerge <- AnimetricsClassifiedFaces %>%
-  mutate(type = "Animetrics",  ID=NA,
+  dplyr::mutate(type = "Animetrics",  ID=NA,
          minX = topLeftX, maxX = topLeftX + width,
          minY = topLeftY, maxY = topLeftY + height) %>%
   dplyr::select(file, type, ID, time.user.self, time.sys.self, time.elapsed, minX, maxX, minY, maxY)  
 SkyBiometryMerge <- SkybiometryClassifiedFaces %>% 
-  mutate(type = "Skybiometry", ID=NA,
+  dplyr::mutate(type = "Skybiometry", ID=NA,
          minX = (center.x - width/2)*8, maxX = (center.x + width/2)*8,
          minY = (center.y - height/2)*4.5, maxY = (center.y + height/2)*4.5) %>%
   dplyr::select(file, type, ID, time.user.self, time.sys.self, time.elapsed, minX, maxX, minY, maxY)  
-GoogleMerge <- GoogleClassifiedFaces %>% 
-  rowwise() %>%
-  mutate(type = "Google", ID=NA,
+GoogleMerge <- GoogleClassifiedFaces %>% rowwise() %>%
+  dplyr::mutate(type = "Google", ID=NA,
          minX = min(boundingPoly.x1, boundingPoly.x4), maxX = min(boundingPoly.x2, boundingPoly.x3),
          minY = min(boundingPoly.y1, boundingPoly.y2), maxY = min(boundingPoly.y3, boundingPoly.y4)) %>%
   ungroup() %>%
-  mutate(minX = ifelse(is.na(minX), 0, minX), minY = ifelse(is.na(minY), 0, minY)) %>%
+  dplyr::mutate(minX = ifelse(is.na(minX), 0, minX), minY = ifelse(is.na(minY), 0, minY)) %>%
   dplyr::select(file, type, ID, time.user.self, time.sys.self, time.elapsed, minX, maxX, minY, maxY) 
 ManualMerge <- ManualClassifiedFaces %>%
-  mutate(type = "Manual")%>%
-  rename(minX = xmin, maxX = xmax,
+  dplyr::mutate(type = "Manual") %>%
+  dplyr::rename(minX = xmin, maxX = xmax,
          minY = ymin, maxY = ymax) %>%
   dplyr::select(file, type, ID, time.user.self, time.sys.self, time.elapsed, minX, maxX, minY, maxY)#%>% filter(file=="2016_RLA_R01_JDuckworth_AUS_vs_LHewitt_AUS_MS150_clip.0056.png")  
 mergedData <- rbind(MicrosoftMerge, AnimetricsMerge, SkyBiometryMerge, GoogleMerge, ManualMerge)
@@ -106,17 +105,18 @@ mergedFaceMatches <- mergedData %>%
 
 #Does it match manual
 mergedFaceMatches <- mergedFaceMatches %>% group_by(file, boxID) %>%
-  mutate(matchesManual = any(type == "Manual"))
+  dplyr::mutate(matchesManual = ifelse(any(type == "Manual"), TRUE, FALSE))
+  
 
 #size of box
-classifiedIMG <- mergedFaceMatches %>% mutate(size = (maxX - minX) * (maxY - minY))
+classifiedIMG <- mergedFaceMatches %>% dplyr::mutate(size = (maxX - minX) * (maxY - minY))
 
 
 ### MERGE ALL THE DATA!
 
 metaManualClassifiedFaces <- merge(dplyr::select(ManualClassifiedFaces, file, facecounter, 
                                                  detect, obscured, lighting, headangle, glasses, visorhat, ID),
-                                   dplyr::select(dplyr::ungroup(dplyr::filter(mergedFaceMatches, type=="Manual")), ID, boxID), by="ID")
+                                   dplyr::select(dplyr::ungroup(dplyr::filter(mergedFaceMatches, type=="Manual")), ID, boxID), by=c("file", "boxID"))
 
 
 
@@ -124,7 +124,8 @@ metaManualClassifiedFaces <- merge(dplyr::select(ManualClassifiedFaces, file, fa
 ## Pairwise merge, always include file, change merge order
 
 
-metaFaces <- merge(metaManualClassifiedFaces, classifiedIMG, by=c("file", "boxID")) %>% dplyr::select(-ID.y) %>% rename(ID = ID.x)
+metaFaces <- full_join(metaManualClassifiedFaces, classifiedIMG, by=c("file", "boxID")) %>% 
+  dplyr::select(-ID.y, ID=ID.x)
 
 ## THIS CAUSES boxID NAs
 ALLmetaIMG <- merge(ManualClassifiedScenes, metaFaces, by="file", all=TRUE)
@@ -138,7 +139,7 @@ ALLmetaIMG <- merge(ALLmetaIMG, fileInfo, by="file")
 
 #Finding duplicates
 ALLmetaIMG <- ALLmetaIMG %>% group_by(file, boxID) %>%
-  mutate(duplicates = duplicated(type))
+  dplyr::mutate(duplicates = duplicated(type))
 
 
 write.csv(ALLmetaIMG,file = "ALLmetaIMG.csv", row.names = FALSE)
