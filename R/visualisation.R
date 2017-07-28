fd97cd15f1cb0f9e4fbcc786fcd34af8871bd751
Steff <srkob1@student.monash.edu>
2017-07-28 17:27:16 +1000

    Added colour scheme

--- R/visualisation.R
+++ R/visualisation.R
@@ -1,22 +1,31 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(RColorBrewer)
+
+# colour scheme
+type.colours <- c(Animetrics = "#B3E2CD",
+                  Google = "#FDCDAC",
+                  Manual = "#CBD5E8",
+                  Microsoft = "#F4CAE4",
+                  Skybiometry = "#E6F5C9")
+
 
 #Load ALLmetaIMG in (from analysis.R)
 ggplot(ALLmetaIMG, aes(x = type)) + geom_bar(position="dodge")
 
 #NA refers to the boxes which don't match manual (hence we don't have info about the image classification)
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(visorhat)), group=type, fill=type)) + geom_bar(position="dodge")
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(glasses)), group=type, fill=type)) + geom_bar(position="dodge")
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(shotangle)), group=type, fill=type)) + geom_bar(position="dodge")
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(graphic)), group=type, fill=type)) + geom_bar(position="dodge")
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(person)), group=type, fill=type)) + geom_bar(position="dodge")
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(situation)), group=type, fill=type)) + geom_bar(position="dodge")
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(bg)), group=type, fill=type)) + geom_bar(position="dodge")
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(detect)), group=type, fill=type)) + geom_bar(position="dodge")
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(obscured)), group=type, fill=type)) + geom_bar(position="dodge")
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(lighting)), group=type, fill=type)) + geom_bar(position="dodge")
-ggplot(ALLmetaIMG, aes(x = factor(x = factor(headangle)), group=type, fill=type)) + geom_bar(position="dodge")
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(visorhat)), group=type, fill=type)) + geom_bar(position="dodge")  + scale_colour_manual(values=type.colours)
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(glasses)), group=type, fill=type)) + geom_bar(position="dodge") + scale_colour_manual(values=type.colours)
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(shotangle)), group=type, fill=type)) + geom_bar(position="dodge") + scale_colour_manual(values=type.colours)
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(graphic)), group=type, fill=type)) + geom_bar(position="dodge") + scale_colour_manual(values=type.colours)
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(person)), group=type, fill=type)) + geom_bar(position="dodge") + scale_colour_manual(values=type.colours)
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(situation)), group=type, fill=type)) + geom_bar(position="dodge") + scale_colour_manual(values=type.colours)
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(bg)), group=type, fill=type)) + geom_bar(position="dodge") + scale_colour_manual(values=type.colours)
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(detect)), group=type, fill=type)) + geom_bar(position="dodge") + scale_colour_manual(values=type.colours)
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(obscured)), group=type, fill=type)) + geom_bar(position="dodge") + scale_colour_manual(values=type.colours)
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(lighting)), group=type, fill=type)) + geom_bar(position="dodge") + scale_colour_manual(values=type.colours)
+ggplot(ALLmetaIMGnamed, aes(x = factor(x = factor(headangle)), group=type, fill=type)) + geom_bar(position="dodge") + scale_colour_manual(values=type.colours)
 
 
 ## TODO: Proportions of manual classifications.
